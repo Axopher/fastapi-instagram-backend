e9414e3ad107f98e26394238ed659d973afd000f
Axopher <amanshres007@gmail.com>
2025-06-17 03:48:01 +0545

    minor changes

--- src/database.py
+++ src/database.py
@@ -1,13 +1,24 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from pathlib import Path
 
-DB_URL = "sqlite:///./db.sql"
+# Load .env from the root directory
+env_path = Path(__file__).resolve().parents[1] / ".env"
+load_dotenv(dotenv_path=env_path)
 
-engine = create_engine(DB_URL, pool_pre_ping=True)
+# Now this will work:
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {},
+    pool_pre_ping=True,
+)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
